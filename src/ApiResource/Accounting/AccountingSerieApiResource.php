6152bca2bad38ea6f68e726d6f3aaaff35684f72
Daniel Subiabre <daniel.subiabre@platoniq.net>
2025-02-10 14:53:05 +0100

    Update description of accounting serie api resource

--- src/ApiResource/Accounting/AccountingSerieApiResource.php
+++ src/ApiResource/Accounting/AccountingSerieApiResource.php
@@ -7,7 +7,14 @@ use App\Dto\AccountingSerieDto;
 use App\State\Accounting\AccountingSerieStateProcessor;
 
 /**
- * An AccountingSerie displays plottable aggregated balance data over a time range for a given Accounting.
+ * An AccountingSerie displays plottable, aggregated balance data over a time range for a given Accounting.\
+ * \
+ * AccountingSeries are calculated in real-time for each request from the available data for the Accountings,
+ * each data point is a Money object representing the aggregated balance of the Accounting at that point in the serie.\
+ * \
+ * Data points are relative to the series and do not relate to any specific unit of time.\
+ * You might get data points for specific days when you make the `maxLength` match the number of days in your date range,
+ * but the behaviour is not guaranteed and data points might be less than your desired output if there is not enough data.
  */
 #[API\ApiResource(shortName: 'AccountingSerie')]
 #[API\Post(
