562235a7b104ee45ddf911c5ba0e3233f99eed83
Daniel Subiabre <daniel.subiabre@platoniq.net>
2025-02-10 14:00:31 +0100

    Update AccountingSerieDto default values

--- src/Dto/AccountingSerieDto.php
+++ src/Dto/AccountingSerieDto.php
@@ -2,13 +2,14 @@
 
 namespace App\Dto;
 
+use ApiPlatform\Metadata\ApiProperty;
 use App\ApiResource\Accounting\AccountingApiResource;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class AccountingSerieDto
 {
     /**
-     * The source Accounting for the data serie. 
+     * The source Accounting for the data serie.
      */
     #[Assert\NotBlank()]
     public AccountingApiResource $accounting;
@@ -22,15 +23,20 @@ class AccountingSerieDto
     /**
      * The end date for the data serie. Inclusive.
      */
-    #[Assert\NotBlank()]
-    #[Assert\LessThanOrEqual('today')]
-    public \DateTimeInterface $dateEnd;
+    #[Assert\LessThanOrEqual('now')]
+    #[ApiProperty(default: 'now')]
+    public ?\DateTimeInterface $dateEnd = null;
 
-        /**
-     * The number of data points in the serie.
+    /**
+     * The max number of data points in the serie.\
+     * Returned data set might have less data points if there is not enough data.
      */
-    #[Assert\NotBlank()]
     #[Assert\Positive()]
-    #[Assert\Range(min: 10, max: 100)]
-    public int $length = 10;
+    #[Assert\Range(min: 1, max: 100)]
+    public int $maxLength = 10;
+
+    public function __construct()
+    {
+        $this->dateEnd = $this->dateEnd ?: new \DateTimeImmutable('now');
+    }
 }
