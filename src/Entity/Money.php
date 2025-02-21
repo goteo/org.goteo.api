a1df984bd1d4c012150df0983c643040c6519f6f
Daniel Subiabre <daniel.subiabre@platoniq.net>
2025-02-21 14:03:05 +0100

    Split entity embeddable money from api resource money

--- src/Entity/Money.php
+++ src/Entity/Money.php
@@ -3,7 +3,6 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * NOT FINANCIALLY SAFE.\
@@ -18,17 +17,13 @@ class Money
      * An amount of currency.\
      * Expressed as the minor unit, e.g: cents, pennies, etc.
      */
-    #[Assert\NotBlank()]
-    #[Assert\Positive()]
-    #[ORM\Column(type: 'integer', nullable: false)]
+    #[ORM\Column(type: 'integer', nullable: true)]
     public readonly int $amount;
 
     /**
      * 3-letter ISO 4217 currency code.
      */
-    #[Assert\NotBlank()]
-    #[Assert\Currency()]
-    #[ORM\Column(type: 'string', nullable: false)]
+    #[ORM\Column(type: 'string', nullable: true)]
     public readonly string $currency;
 
     public function __construct(
