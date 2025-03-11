bef17962465f74ab337c236d132093113bbe2dc4
cmorenov1402 <cmoreno@vailos.com>
2025-03-11 11:50:16 +0100

    Create Gateway Charge Extension

---
+++ src/Doctrine/GatewayChargeExtension.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Doctrine;
+
+use ApiPlatform\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+use ApiPlatform\Doctrine\Orm\Extension\QueryItemExtensionInterface;
+use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use App\Entity\Gateway\Charge;
+use Doctrine\ORM\QueryBuilder;
+use Symfony\Bundle\SecurityBundle\Security;
+
+final class GatewayChargeExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
+{
+
+    private Security $security;
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    /**
+     *  Method that applies the filter
+     */
+    public function addFilter(QueryBuilder $queryBuilder, string $resourceClass)
+    {
+        if ($resourceClass !== Charge::class) {
+            return; // Just apply the filter to GatewayCharge
+        }
+
+        $user = $this->security->getUser();
+
+        if (!$user || !$user instanceof \App\Entity\User\User) {
+            return; // If the user is not authenticated or not an instance of User, we do not apply the filter
+        }      
+
+        $rootAlias = $queryBuilder->getRootAliases()[0];
+
+        // Filter by the user based on the relationship with Checkout or Accounting
+        $queryBuilder
+            ->leftJoin("$rootAlias.checkout", "c") // Relationship with Gatewaycheckout
+            ->leftJoin("c.origin", "co") // Relationship with Accounting
+            ->leftJoin("co.user", "u") // Relationship with User
+            ->andWhere("u.id = :userId") // Filter by the user in the Origin entity (accounting)
+            ->setParameter('userId', $user->getId()); // Establishes the authenticated user parameter
+    }
+
+    public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, \ApiPlatform\Metadata\Operation|null $operation = null, array $context = []): void
+    {
+        $this->addFilter($queryBuilder, $resourceClass);
+    }
+
+    public function applyToItem(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, array $identifiers, \ApiPlatform\Metadata\Operation|null $operation = null, array $context = []): void
+    {
+        $this->addFilter($queryBuilder, $resourceClass);
+    }
+}
