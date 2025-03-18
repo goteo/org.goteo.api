efe58e6c997216155c69c01028be58e5c808c070
cmorenov1402 <cmoreno@vailos.com>
2025-03-18 12:35:17 +0100

    Add edit restriction to no owner

--- src/State/Project/SupportStateProcessor.php
+++ src/State/Project/SupportStateProcessor.php
@@ -4,11 +4,21 @@ namespace App\State\Project;
 
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
+use App\Service\Auth\AuthService;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 
 class SupportStateProcessor implements ProcessorInterface
 {
+    public function __construct(private AuthService $authService) {}
+
     public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = [])
     {
+        $owner = $this->authService->getUser();
+
+        if (!$owner) {
+            throw new AuthenticationException();
+        }
+
         return $data;
     }
 }
