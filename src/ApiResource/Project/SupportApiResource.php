0363b620f3afbc825f1137c9f8746a51e87fa871
cmorenov1402 <cmoreno@vailos.com>
2025-03-18 17:23:08 +0100

    Add owner hide when anonymous

--- src/ApiResource/Project/SupportApiResource.php
+++ src/ApiResource/Project/SupportApiResource.php
@@ -16,11 +16,13 @@ use Symfony\Component\Validator\Constraints as Assert;
     shortName: 'ProjectSupport',
     stateOptions: new Options(entityClass: Support::class),
     provider: ApiResourceStateProvider::class,
-    processor: SupportStateProcessor::class,
 )]
 #[API\GetCollection()]
 #[API\Get()]
-#[API\Patch(security: 'is_granted("SUPPORT_EDIT", previous_object)')]
+#[API\Patch(
+    security: 'is_granted("SUPPORT_EDIT", previous_object)',
+    processor: SupportStateProcessor::class
+)]
 class SupportApiResource
 {
     #[API\ApiProperty(identifier: true, writable: false)]
@@ -31,7 +33,10 @@ class SupportApiResource
      * \
      * When `anonymous` is *false* it will only be public to admins and the User.
      */
-    #[API\ApiProperty(writable: false)]
+    #[API\ApiProperty(
+        writable: false,
+        security: 'object and (object.anonymous == false or is_granted("ROLE_ADMIN") or object.owner == user)'
+    )]
     #[API\ApiFilter(filterClass: SearchFilter::class, strategy: 'exact')]
     public UserApiResource $owner;
 
--- src/State/Project/SupportStateProcessor.php
+++ src/State/Project/SupportStateProcessor.php
@@ -4,21 +4,38 @@ namespace App\State\Project;
 
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
+use App\ApiResource\Project\SupportApiResource;
+use App\Entity\Project\Support;
+use App\Mapping\AutoMapper;
 use App\Service\Auth\AuthService;
+use App\State\EntityStateProcessor;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 
 class SupportStateProcessor implements ProcessorInterface
 {
-    public function __construct(private AuthService $authService) {}
+    public function __construct(
+        private EntityStateProcessor $entityStateProcessor,
+        private AuthService $authService,
+        private AutoMapper $autoMapper
+    ) {}
 
     public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = [])
     {
-        $owner = $this->authService->getUser();
+        $user = $this->authService->getUser();
 
-        if (!$owner) {
+        if (!$user) {
             throw new AuthenticationException();
         }
 
-        return $data;
+        /** @var Support */
+        $support = $this->autoMapper->map($data, Support::class);
+
+        $support = $this->entityStateProcessor->process($support, $operation, $uriVariables, $context);
+
+        if ($support === null) {
+            return null;
+        }
+
+        return $this->autoMapper->map($support, SupportApiResource::class);
     }
 }
