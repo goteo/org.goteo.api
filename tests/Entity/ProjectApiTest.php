4500ebc24dec2a7a49194671ff78451094978874
Daniel Subiabre <daniel.subiabre@platoniq.net>
2025-03-26 14:19:00 +0100

    Fix project code in tests

--- tests/Entity/ProjectApiTest.php
+++ tests/Entity/ProjectApiTest.php
@@ -3,8 +3,8 @@
 namespace App\Tests\Entity;
 
 use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
-use App\Entity\Project\Category;
 use App\Entity\Project\Project;
+use App\Entity\Project\ProjectCategory;
 use App\Entity\Project\ProjectDeadline;
 use App\Entity\Project\ProjectStatus;
 use App\Entity\Project\ProjectTerritory;
@@ -45,7 +45,7 @@ class ProjectApiTest extends ApiTestCase
         $project->setTitle('Test Project');
         $project->setSubtitle('Test Project Subtitle');
         $project->setDeadline(ProjectDeadline::Minimum);
-        $project->setCategory(Category::LibreSoftware);
+        $project->setCategory(ProjectCategory::LibreSoftware);
         $project->setDescription('Test Project Description');
         $project->setTerritory(new ProjectTerritory('ES'));
         $project->setOwner($owner);
