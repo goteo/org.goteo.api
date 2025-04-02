8d47357f617a87263ea84f305234e2663af3a23e
Daniel Subiabre <daniel.subiabre@platoniq.net>
2025-04-02 16:22:24 +0200

    Add validation: available reward units for claim

---
+++ src/Validator/AvailableRewardUnitsValidator.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Validator;
+
+use App\ApiResource\Project\RewardApiResource;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+final class AvailableRewardUnitsValidator extends ConstraintValidator
+{
+    /**
+     * @param RewardApiResource $value
+     */
+    public function validate(mixed $value, Constraint $constraint): void
+    {
+        if ($value->hasUnits && $value->unitsAvailable > 0) {
+            return;
+        }
+
+        if ($value === null || $value === '') {
+            return;
+        }
+
+        $this->context->buildViolation($constraint->message)
+            ->setParameter('{{ id }}', $value->id)
+            ->addViolation()
+        ;
+    }
+}
