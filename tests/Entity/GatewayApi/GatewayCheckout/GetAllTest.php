8c149d5836f1214a3caf0d5880682cd240ad51df
cmorenov1402 <cmoreno@vailos.com>
2025-04-03 11:39:52 +0200

    Remove dead code in gateway checkout get all test class

--- tests/Entity/GatewayApi/GatewayCheckout/GetAllTest.php
+++ tests/Entity/GatewayApi/GatewayCheckout/GetAllTest.php
@@ -4,14 +4,9 @@ namespace App\Tests\Entity\GatewayApi\GatewayCheckout;
 
 use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
 use App\Factory\Gateway\CheckoutFactory;
-use App\Factory\Project\ProjectFactory;
 use App\Factory\User\UserFactory;
-use App\Gateway\Wallet\WalletService;
-use App\Library\Economy\MoneyService;
-use App\Service\Gateway\CheckoutService;
 use App\Tests\Traits\TestHelperTrait;
 use Doctrine\Common\DataFixtures\Purger\ORMPurger;
-use Doctrine\ORM\EntityManagerInterface;
 use Zenstruck\Foundry\Test\Factories;
 use Zenstruck\Foundry\Test\ResetDatabase;
 
@@ -40,58 +35,15 @@ class GetAllTest extends ApiTestCase
 
     private static function loadCheckouts()
     {
-        $user = UserFactory::createOne([
+        UserFactory::createOne([
             'email' => self::USER_EMAIL,
             'password' => self::USER_PASSWORD,
         ]);
 
-        // $otherUser = UserFactory::createOne([
-        //     'handle' => 'other_user',
-        //     'email' => self::OTHER_USER_EMAIL,
-        //     'password' => self::USER_PASSWORD
-        // ]);
-
-        // $project = ProjectFactory::createOne(['owner' => $otherUser]);
-
-        CheckoutFactory::createOne([
-            // 'origin' => $user->getAccounting(),
-            // 'target' => $project->getAccounting()
-        ]);
-
-        // $container = self::getContainer();
-
-        // $gatewayFactory = new GatewayCheckoutFactory(
-        //     $container->get(WalletService::class),
-        //     $container->get(MoneyService::class),
-        //     $container->get(CheckoutService::class),
-        //     $container->get(EntityManagerInterface::class)
-        // );
-        // $gatewayFactory->createOne([
-        //     'origin' => $user->getAccounting(),
-        //     'target' => $project->getAccounting()
-        // ]);
-
-        //         $container = self::getContainer();
-
-        // GatewayCheckoutFactory::setServices(
-        //     $container->get(WalletService::class),
-        //     $container->get(MoneyService::class),
-        //     $container->get(CheckoutService::class),
-        //     $container->get(EntityManagerInterface::class)
-        // );
-
-        // GatewayCheckoutFactory::new()->withoutPersisting()->createOne([
-        //     'origin' => $user->getAccounting(),
-        //     'target' => $project->getAccounting()
-        // ]);
+        CheckoutFactory::createOne();
     }
 
     // Runable tests
-
-    // public function testSetup()
-    // {
-    //     $this->assertArrayHasKey('test', ['test' => 'some value']);
-    // }
 
     public function testGetAllSuccessful()
     {
