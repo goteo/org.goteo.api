db77ac96e482f4936fd6bd07d22c9eeb2e419821
cmorenov1402 <cmoreno@vailos.com>
2025-04-04 10:13:32 +0200

    Add create with empty gateway field test

--- tests/Entity/GatewayApi/GatewayCheckout/CreateTest.php
+++ tests/Entity/GatewayApi/GatewayCheckout/CreateTest.php
@@ -9,9 +9,27 @@ class CreateTest extends BaseTest
 {
     protected const METHOD = 'POST';
 
+    private const DATA = [
+        'gateway' => '/v4/gateways/stripe',
+        'origin' => '/v4/accountings/1',
+        'charges' => [
+            [
+                'type' => 'single',
+                'title' => 'Charge 1',
+                'target' => '/v4/accountings/3',
+                'money' => [
+                    'amount' => 100,
+                    'currency' => 'USD',
+                ],
+                'description' => 'description',
+            ],
+        ],
+        'returnUrl' => 'https://example.com/return',
+    ];
+
     // Auxiliary functions
 
-    private function makeRequest($data): Client
+    private function makeRequest(array $data = self::DATA): Client
     {
         $client = static::createClient();
         $client->request(
@@ -30,29 +48,22 @@ class CreateTest extends BaseTest
 
     public function testCreateSuccessful()
     {
-        $data = [
-            'gateway' => '/v4/gateways/stripe',
-            'origin' => '/v4/accountings/1',
-            'charges' => [
-                [
-                    'type' => 'single',
-                    'title' => 'Charge 1',
-                    'target' => '/v4/accountings/3',
-                    'money' => [
-                        'amount' => 100,
-                        'currency' => 'USD',
-                    ],
-                    'description' => 'description',
-                ],
-            ],
-            'returnUrl' => 'https://example.com/return',
-        ];
-
-        $client = $this->makeRequest($data);
+        $client = $this->makeRequest();
 
         $this->assertResponseStatusCodeSame(Response::HTTP_CREATED);
 
         $responseData = $this->getResponseData($client);
         $this->assertCheckoutIsCorrect($responseData);
+    }
+
+    public function testCreateWithEmptyGatewayField()
+    {
+        $data = array_merge(self::DATA, [
+            'gateway' => '',
+        ]);
+
+        $this->makeRequest($data);
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_BAD_REQUEST);
     }
 }
