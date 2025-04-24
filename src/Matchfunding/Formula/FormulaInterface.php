391972c46b2988c2718a18019de7b3147e47863f
Daniel Subiabre <daniel.subiabre@platoniq.net>
2025-04-24 13:57:45 +0200

    Update FormulaInterface

--- src/Matchfunding/Formula/FormulaInterface.php
+++ src/Matchfunding/Formula/FormulaInterface.php
@@ -3,6 +3,7 @@
 namespace App\Matchfunding\Formula;
 
 use App\Entity\Money;
+use Brick\Math\BigNumber;
 
 interface FormulaInterface
 {
@@ -12,11 +13,18 @@ interface FormulaInterface
     public static function getName(): string;
 
     /**
+     * The underlying math expressed as a common-notation formula.
+     */
+    public static function getAsExpression(): string;
+
+    /**
      * Process an input Money and obtain a delta Money, which is the Money to be given by the Matcher.
      *
-     * @param Money $money The Money input that is to be matched
+     * @param BigNumber $factor A matching factor for the formula to use
+     * @param Money     $money  The input Money that is to be matched
+     * @param Money     $limit  An upper bound for the matching formula, the delta won't be higher than this
      *
-     * @return Money A Money delta
+     * @return Money The output Money delta
      */
-    public function match(Money $money): Money;
+    public function match(BigNumber $factor, Money $money, Money $limit): Money;
 }
