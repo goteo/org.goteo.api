de64cce49a4d669d932279b798211164461e902d
Daniel Subiabre <daniel.subiabre@platoniq.net>
2025-05-02 15:03:45 +0200

    Added dama/doctrine-test-bundle package

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Goteo\Benzina\BenzinaBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
