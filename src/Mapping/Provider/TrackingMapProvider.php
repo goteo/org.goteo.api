a5db0caf5b5d224b55ef6fba7f92d7f6b7d9d63b
Daniel Subiabre <daniel.subiabre@platoniq.net>
2025-05-22 13:23:29 +0200

    Make Checkout Trackings into their own entity

---
+++ src/Mapping/Provider/TrackingMapProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Mapping\Provider;
+
+use App\Gateway\Tracking;
+use App\Repository\Gateway\TrackingRepository;
+use AutoMapper\Provider\ProviderInterface;
+
+class TrackingMapProvider implements ProviderInterface
+{
+    public function __construct(
+        private TrackingRepository $trackingRepository,
+    ) {}
+
+    /**
+     * @param Tracking $source
+     */
+    public function provide(string $targetType, mixed $source, array $context): object|array|null
+    {
+        return $this->trackingRepository->findOneBy([
+            'title' => $source->title,
+            'value' => $source->value
+        ]);
+    }
+}
