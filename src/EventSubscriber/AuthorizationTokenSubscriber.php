06643724f6b62c45be5193cc5ad2d9d3534596d7
Daniel Subiabre <daniel.subiabre@platoniq.net>
2025-09-04 19:19:59 +0200

    Set and remove authToken cookie on token post/delete

---
+++ src/EventSubscriber/AuthorizationTokenSubscriber.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use ApiPlatform\Symfony\EventListener\EventPriorities;
+use App\ApiResource\User\UserTokenApiResource;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+final class AuthorizationTokenSubscriber implements EventSubscriberInterface
+{
+    private const COOKIE_NAME = 'authToken';
+    private const COOKIE_TTL = 86400;
+
+    public function __construct() {}
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::VIEW => ['onView', EventPriorities::POST_WRITE],
+            KernelEvents::RESPONSE => ['onResponse', EventPriorities::POST_RESPOND],
+        ];
+    }
+
+    private function getCookieTtl(): \DateTimeInterface
+    {
+        return (new \DateTime())->add(new \DateInterval(\sprintf('PT%dS', self::COOKIE_TTL)));
+    }
+
+    private function getCookie(string $tokenValue)
+    {
+        return new Cookie(
+            name: self::COOKIE_NAME,
+            value: $tokenValue,
+            expire: $this->getCookieTtl(),
+            path: '/',
+            secure: false,
+            httpOnly: true,
+            sameSite: 'Lax'
+        );
+    }
+
+    public function onView(ViewEvent $event): void
+    {
+        $resourceClass = $event->getRequest()->attributes->get('_api_resource_class');
+
+        if ($resourceClass !== UserTokenApiResource::class) {
+            return;
+        }
+
+        $event->getRequest()->attributes->set('authTokenMethod', $event->getRequest()->getMethod());
+
+        if ($event->getRequest()->getMethod() === Request::METHOD_POST) {
+            $token = $event->getControllerResult();
+
+            $event->getRequest()->attributes->set('authTokenValue', $token->token);
+        }
+    }
+
+    public function onResponse(ResponseEvent $event): void
+    {
+        $token = $event->getRequest()->attributes->get('authTokenValue');
+        $method = $event->getRequest()->attributes->get('authTokenMethod');
+
+        if (!$method) {
+            return;
+        }
+
+        switch ($method) {
+            case Request::METHOD_DELETE:
+                $event->getResponse()->headers->clearCookie(self::COOKIE_NAME);
+                break;
+            case Request::METHOD_POST:
+                $event->getResponse()->headers->setCookie($this->getCookie($token));
+                break;
+        }
+    }
+}
