f519aa1b094c93397698fa7a93ed5330a4919872
Daniel Subiabre <daniel.subiabre@platoniq.net>
2025-09-10 06:40:22 +0200

    Update custom ProjectReward validators

---
+++ src/Validator/SingleRewardClaimPerCharge.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Validator;
+
+use Symfony\Component\Validator\Constraint;
+
+#[\Attribute(\Attribute::TARGET_CLASS)]
+final class SingleRewardClaimPerCharge extends Constraint
+{
+    public string $message = 'There is an existing ProjectRewardClaim for that Charge.';
+
+    public function __construct(
+        public string $mode = 'strict',
+        ?array $groups = null,
+        mixed $payload = null,
+    ) {
+        parent::__construct([], $groups, $payload);
+    }
+
+    public function getTargets()
+    {
+        return self::CLASS_CONSTRAINT;
+    }
+}
