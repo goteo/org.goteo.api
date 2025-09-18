0e49b7802a1967abf08105700359f23434312f05
Daniel Subiabre <daniel.subiabre@platoniq.net>
2025-09-18 17:59:39 +0200

    Use Transformer for AccountingApiResource::owner

--- src/ApiResource/Accounting/AccountingApiResource.php
+++ src/ApiResource/Accounting/AccountingApiResource.php
@@ -4,17 +4,11 @@ namespace App\ApiResource\Accounting;
 
 use ApiPlatform\Doctrine\Orm\State\Options;
 use ApiPlatform\Metadata as API;
-use App\ApiResource\Matchfunding\MatchCallApiResource;
-use App\ApiResource\Project\ProjectApiResource;
-use App\ApiResource\TipjarApiResource;
-use App\ApiResource\User\UserApiResource;
 use App\Entity\Accounting\Accounting;
-use App\Entity\Matchfunding\MatchCall;
-use App\Entity\Project\Project;
-use App\Entity\Tipjar;
-use App\Entity\User\User;
+use App\Mapping\Transformer\AccountingOwnerMapTransformer;
 use App\State\Accounting\AccountingStateProcessor;
 use App\State\Accounting\AccountingStateProvider;
+use AutoMapper\Attribute\MapFrom;
 
 /**
  * v4 features an advanced economy model under the hood.
@@ -43,39 +37,9 @@ class AccountingApiResource
      */
     public string $currency;
 
-    #[API\ApiProperty(readable: false, writable: false)]
-    public string $ownerClass;
-
-    #[API\ApiProperty(readable: false, writable: false)]
-    public ?UserApiResource $user = null;
-
-    #[API\ApiProperty(readable: false, writable: false)]
-    public ?ProjectApiResource $project = null;
-
-    #[API\ApiProperty(readable: false, writable: false)]
-    public ?TipjarApiResource $tipjar = null;
-
-    #[API\ApiProperty(readable: false, writable: false)]
-    public ?MatchCallApiResource $matchCall;
-
     /**
-     * The resource owning this Accounting.
-     *
-     * @return UserApiResource|ProjectApiResource|TipjarApiResource
+     * IRI of the resource owning this Accounting.
      */
-    public function getOwner(): ?object
-    {
-        switch ($this->ownerClass) {
-            case User::class:
-                return $this->user;
-            case Project::class:
-                return $this->project;
-            case Tipjar::class:
-                return $this->tipjar;
-            case MatchCall::class:
-                return $this->matchCall;
-        }
-
-        return null;
-    }
+    #[MapFrom(transformer: AccountingOwnerMapTransformer::class)]
+    public string $owner;
 }
