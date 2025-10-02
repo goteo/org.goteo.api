ecc78d393f7fc9e9797d397e981f0465ad1b507b
Daniel Subiabre <daniel.subiabre@platoniq.net>
2025-10-02 13:15:52 +0200

    Figx categories trying to be created on POST /projects

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -2,9 +2,12 @@
 
 namespace App\Entity;
 
+use App\Mapping\Provider\EntityMapProvider;
 use App\Repository\CategoryRepository;
+use AutoMapper\Attribute\MapProvider;
 use Doctrine\ORM\Mapping as ORM;
 
+#[MapProvider(EntityMapProvider::class)]
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
 class Category
 {
