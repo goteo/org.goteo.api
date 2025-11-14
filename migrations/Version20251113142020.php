e4dd1b8ebd2685e02d874ff63cd72677a67572be
Daniel Subiabre <daniel.subiabre@platoniq.net>
2025-11-14 09:50:18 +0100

    feat: add migration version

---
+++ migrations/Version20251113142020.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20251113142020 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return "";
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql("UPDATE project SET `status` = 'funding.paid' WHERE `status` = 'funded'");
+        $this->addSql("UPDATE project SET `status` = 'in_campaign_review.request_change' WHERE `status` = 'in_editing'");
+        $this->addSql("UPDATE project SET `status` = 'in_campaign_review' WHERE `status` = 'in_review'");
+        $this->addSql("UPDATE project SET `status` = 'campaign.failed' WHERE `status` = 'unfunded'");
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql("UPDATE project SET `status` = 'funded' WHERE `status` = 'funding.paid'");
+        $this->addSql("UPDATE project SET `status` = 'in_editing' WHERE `status` = 'in_campaign_review.request_change'");
+        $this->addSql("UPDATE project SET `status` = 'in_review' WHERE `status` = 'in_campaign_review'");
+        $this->addSql("UPDATE project SET `status` = 'unfunded' WHERE `status` = 'campaign.failed'");
+    }
+}
