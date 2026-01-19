1919665e77753011aa2da9d8293fd60a02f69817
Daniel Subiabre <daniel.subiabre@platoniq.net>
2026-01-19 13:23:14 +0100

    feat: improve user handle creation

--- src/Service/UserService.php
+++ src/Service/UserService.php
@@ -7,25 +7,29 @@ class UserService
     /**
      * Normalizes a string into a handle-valid string.
      *
+     * @param string $value The input string to be normalized into a valid handle
+     * @param int    $min   The shortest possible length of the output string
+     * @param int    $max   The longest possible length of the output string
+     *
      * @return string A handle-valid string
      *
      * @throws \Exception If the given value could not be normalized into a compliant handle string
      */
-    public static function asHandle(string $value): string
+    public static function asHandle(string $value, int $min = 4, int $max = 30): string
     {
+        $value = \strtolower($value);
+
         // If email remove provider
-        if (\str_contains($value, '@') && \preg_match('/^[^@]+/', $value, $matches)) {
-            $value = $matches[0];
-        }
+        $value = \preg_replace('/@.*[\.-](com|net|org|es)/', '', $value);
+
+        // Replace known subs
+        $value = \str_replace(['&', '@', 'ñ', '=', '≠'], ['and', 'at', 'nn', 'eq', 'nq'], $value);
 
         // Only lowercase a-z, numbers, underscore and middle dots in user handles
         $value = \preg_replace('/[^a-z0-9_.]|^\.|\.$/', '_', \strtolower($value));
 
-        // Min length 4
-        $value = \str_pad($value, 4, '_');
-
-        // Max length 30
-        $value = \substr($value, 0, 30);
+        $value = \str_pad($value, $min, \hash('md5', $value));
+        $value = \substr($value, 0, $max);
 
         if (strlen(str_replace('_', '', $value)) < 1) {
             throw new \Exception(\sprintf('The string \'%s\' could not be safely normalized for a handle', $value));
