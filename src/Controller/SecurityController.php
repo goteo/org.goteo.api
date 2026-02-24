ce6ab7b8103f3cbe8d47d51907ba4e457387ca31
Daniel Subiabre <daniel.subiabre@platoniq.net>
2026-02-24 17:33:59 +0100

    feat: update auth paths

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
+#[Route('/security')]
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
