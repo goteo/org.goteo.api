7a8d9291560f118e0d9049893dc1cf1bc5a3af97
Daniel Subiabre <daniel.subiabre@platoniq.net>
2026-05-22 15:34:47 +0200

    fix: improve error handling in SupportDisplayNameMapTransformer

--- src/Mapping/Transformer/SupportDisplayNameMapTransformer.php
+++ src/Mapping/Transformer/SupportDisplayNameMapTransformer.php
@@ -2,6 +2,7 @@
 
 namespace App\Mapping\Transformer;
 
+use App\Entity\Accounting\AccountingOwnerInterface;
 use App\Entity\Matchfunding\MatchCall;
 use App\Entity\Project\Support;
 use App\Entity\User\User;
@@ -18,9 +19,28 @@ class SupportDisplayNameMapTransformer implements PropertyTransformerInterface
      */
     public function transform(mixed $value, object|array $source, array $context): mixed
     {
-        return match ($source->getOrigin()->getOwnerClass()) {
-            User::class => $this->userDisplayNameMapTransformer->transform($value, $source->getOrigin()->getUser(), $context),
-            MatchCall::class => $source->getOrigin()->getMatchCall()->getTitle(),
+        $origin = $source->getOrigin();
+        if (!$origin) {
+            throw new \Exception(\sprintf('The Support with ID %s does not have an origin', $source->getId()));
+        }
+
+        return match ($origin->getOwnerClass()) {
+            User::class => $this->userDisplayNameMapTransformer->transform($value, $origin->getUser(), $context),
+            MatchCall::class => $origin->getMatchCall()->getTitle(),
+            default => $this->attemptDisplayName($origin->getOwner()),
         };
+    }
+
+    private function attemptDisplayName(AccountingOwnerInterface $owner): string
+    {
+        if (\method_exists($owner, 'getTitle')) {
+            return $owner->getTitle();
+        }
+
+        if (\method_exists($owner, 'getName')) {
+            return $owner->getName();
+        }
+
+        throw new \Exception(\sprintf("AccountingOwnerInterface '%s' does not provide a displayable name", $owner::class));
     }
 }
